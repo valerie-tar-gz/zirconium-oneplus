ca290ffbc4eaaa8ed3c557cf6e956a452ce88d1b
Valerie <155010216+valerie-tar-gz@users.noreply.github.com>
2026-01-14 12:46:08 +0000

    Add SDM845 kernel from pocketblue copr

--- build_files/00-mobilise.sh
+++ build_files/00-mobilise.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+mkdir /boot/dtb
+dnf -y remove \
+    kernel \
+    kernel-core \
+    kernel-modules \
+    kernel-modules-core
+rm -rf /usr/lib/modules/*
+dnf -y copr enable pocketblue/sdm845
+dnf -y copr disable pocketblue/sdm845
+dnf -y --enablerepo copr:copr.fedorainfracloud.org:pocketblue:sdm845 install \
+    kernel \
+    kernel-modules-extra
+rm -rf /boot/dtb
