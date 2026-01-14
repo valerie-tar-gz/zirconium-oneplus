b2e6e46caa7a167767af2d70a57e44d3ed8e1f31
Valerie <155010216+valerie-tar-gz@users.noreply.github.com>
2026-01-14 13:09:11 +0000

    Add Kernel stuff because I'm a stupid chud and don't know if I need it

--- build_files/00-mobilise.sh
+++ build_files/00-mobilise.sh
@@ -13,3 +13,8 @@ dnf -y --enablerepo copr:copr.fedorainfracloud.org:pocketblue:sdm845 install \
     kernel \
     kernel-modules-extra
 rm -rf /boot/dtb
+
+KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
+export DRACUT_NO_XATTR=1
+dracut --no-hostonly --kver "$KERNEL_VERSION" --reproducible --zstd -v --add ostree -f "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
+chmod 0600 "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
