0ca3af7f491d506e364f607df54c34f282646ac1
Valerie <155010216+valerie-tar-gz@users.noreply.github.com>
2026-01-14 15:19:56 +0000

    Slop because I'm stupid

--- build_files/00-mobilise.sh
+++ build_files/00-mobilise.sh
@@ -79,6 +79,7 @@ dnf -y copr disable pocketblue/common
 dnf -y copr disable pocketblue/extra
 
 KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
+depmod "${KERNEL_VERSION}"
 export DRACUT_NO_XATTR=1
 dracut --no-hostonly --kver "$KERNEL_VERSION" --reproducible --zstd -v --add ostree -f "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
 chmod 0600 "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
