dfc4adb4b5a85b408f0320d97ae9ba10072797c8
Valerie <155010216+valerie-tar-gz@users.noreply.github.com>
2026-01-16 04:41:53 +0000

    Add DNF flag for the kernel

--- build_files/00-mobilise.sh
+++ build_files/00-mobilise.sh
@@ -78,7 +78,7 @@ printf '%s\n' '#!/bin/sh' 'exit 0' > 50-dracut.install
 chmod +x  05-rpmostree.install 50-dracut.install
 popd
 
-dnf -y install kernel kernel-modules-extra --repo copr:copr.fedorainfracloud.org:pocketblue:sdm845
+dnf -y install kernel kernel-modules-extra --repo copr:copr.fedorainfracloud.org:pocketblue:sdm845 --setopt=tsflags=noscripts
 
 rm -rf /boot/dtb
 
